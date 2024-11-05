54efb282edbc392a85d8925d3376b25f1a608ad3
Kadestery <k_keati@live.concordia.ca>
2024-11-05 18:09:30 -0500

    Reapply "Merge branch 'dev' into 51/UI-reset-option"
    This reverts commit 76731d2f176c4db71b6755a1950027d73154df29.

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -3,8 +3,11 @@
 'use client';
 
 import React, { useState } from 'react';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 import LoadingOverlay from './components/LoadingOverlay';
+import MapView from './components/MapView';
+import { MapLayerProvider } from './components/MapContext';
+import Sidebar from './components/Sidebar';
 import TopLeftButtons from './components/TopLeftButtons';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
@@ -12,38 +15,42 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [progress, setProgress] = useState(0);
 
   const updateWildfireLayer = (layerName: string) => {
-    setLoading(true);
-    setProgress(0); // Reset progress when starting a new load
+    // setLoading(true);
+    // setProgress(0); // Reset progress when starting a new load
 
-    // Mock loading progress
-    const interval = setInterval(() => {
-      setProgress((prev) => {
-        if (prev >= 100) {
-          clearInterval(interval);
-          setLoading(false); // Hide loading overlay after loading completes
-          return 100;
-        }
-        return prev + 10;
-      });
-    }, 300);
+    // // Mock loading progress
+    // const interval = setInterval(() => {
+    //   setProgress((prev) => {
+    //     if (prev >= 100) {
+    //       clearInterval(interval);
+    //       setLoading(false); // Hide loading overlay after loading completes
+    //       return 100;
+    //     }
+    //     return prev + 10;
+    //   });
+    // }, 300);
   };
-
+  
   return (
-    <html lang="en">
-    <body>
-    <TopLeftButtons />
+    <MapLayerProvider>
+      
+      <html lang="en">
+      <body>
+      <TopLeftButtons />
     <div className="layout-container relative">
-      <LoadingOverlay progress={progress} isVisible={loading} />
-      <header>
-        <Navbar updateWildfireLayer={updateWildfireLayer} />
-      </header>
-      <main className="app-main">{children}</main>
-      <footer className="app-footer">
-        <p>&copy; 2024 Wildfire Visualization Project</p>
-      </footer>
-    </div>
-    </body>
-    </html>
+        <LoadingOverlay progress={progress} isVisible={loading} />
+        <header>
+          {/* <Navbar /> */}
+        </header>
+        <main className="app-main">{children}</main>
+        <MapView />
+        <Sidebar />
+        <footer className="app-footer">
+        </footer>
+      </div>
+      </body>
+      </html>
+      </MapLayerProvider>
   );
 };
 
