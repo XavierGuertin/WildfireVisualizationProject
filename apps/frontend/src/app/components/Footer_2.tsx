ed0b26b5635a42e5e0d24c744285b9bc326d4fd8
lo-vil <louis.villemure@gmail.com>
2024-11-06 12:09:26 -0500

    slider with play, pause, and stop. also added control speeds

--- apps/frontend/src/app/components/Footer_2.tsx
+++ apps/frontend/src/app/components/Footer_2.tsx
@@ -1,17 +1,33 @@
 // 'use client';
 
 import React, { useState } from "react";
-import '../styles/global.css';
+import '../styles/footer.css';
+import { FaPlayCircle, FaPauseCircle, FaStopCircle } from "react-icons/fa";
 
 const footer_2 = () => {
 
     const [sliderValue, setSliderValue] = useState(0);
+    const [play, setPlay] = useState(false);
 
     const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSliderValue(Number(event.target.value));
     };
 
+    const handlePlayPress = () => {
+        setPlay(true);
+    };
+
+    const handlePausePress = () => {
+        setPlay(false);
+    };
+
+    const handleStopPress = () => {
+        setPlay(false);
+        setSliderValue(0);
+    };
+
     return (
+        <>
         <div className="footerContainer" >
             <div className="sliderContainer">
                 <input
@@ -22,11 +38,21 @@ const footer_2 = () => {
                     value={sliderValue}
                     onChange={handleSliderChange}
                 />
+                <button className="iconButton" onClick={handlePlayPress}><FaPlayCircle className="controlIcon" size={25} /></button>
+                <button className="iconButton" onClick={handlePausePress}><FaPauseCircle className="controlIcon" size={25} /></button>
+                <button className="iconButton" onClick={handleStopPress}><FaStopCircle className="controlIcon" size={25} /></button>
             </div>
             <div className="speedContainer">
-
+                <p>Speed:</p>
+                <p>0.25x</p>
+                <p>0.5x</p>
+                <p>1x</p>
+                <p>1.25x</p>
+                <p>1.5x</p>
+                <p>2x</p>
             </div>
         </div>
+        </>
     );
 }
 
--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -45,7 +45,9 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
         </header>
         <main className="app-main">{children}</main>
         <MapView />
+        <p>Louis</p>
         <Sidebar />
+        <p>Louis</p>
         <Footer_2 />
         {/* <Footer /> */}
         <footer className="app-footer">
