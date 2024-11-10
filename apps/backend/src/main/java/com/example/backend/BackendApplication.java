94edc7ab02678619a80fa59621d2dc5e71b76e94
justncheng <chengjustin2002@gmail.com>
2024-11-10 01:15:20 -0500

    Added logging dependencies, properties, log4j2-spring.xml file, and example logs

--- apps/backend/src/main/java/com/example/backend/BackendApplication.java
+++ apps/backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,25 @@
 package com.example.backend;
 
-import org.springframework.boot.CommandLineRunner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
+
+  private static final Logger logger = LogManager.getLogger(BackendApplication.class);
   public static void main(String[] args) {
     SpringApplication.run(BackendApplication.class, args);
+
+    logger.debug("Debug Log!");
+    logger.info("Info Log!");
+    logger.warn("Warning Log!");
+    logger.error("Error Log!");
+    logger.fatal("Fatal Log!");
+
   }
 }
