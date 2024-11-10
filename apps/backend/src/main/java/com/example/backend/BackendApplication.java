a5214bba23701813e4ec6980cfbeb82282fba6f7
justncheng <chengjustin2002@gmail.com>
2024-11-10 10:47:02 -0500

    Commented out logger types

--- apps/backend/src/main/java/com/example/backend/BackendApplication.java
+++ apps/backend/src/main/java/com/example/backend/BackendApplication.java
@@ -8,18 +8,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.context.annotation.Bean;
 
+/*
+ * Various types of logs:
+ * logger.debug("Debug Log!");
+ * logger.info("Info Log!");
+ * logger.warn("Warning Log!");
+ * logger.error("Error Log!");
+ * logger.fatal("Fatal Log!");
+ */
+
 @SpringBootApplication
 public class BackendApplication {
 
   private static final Logger logger = LogManager.getLogger(BackendApplication.class);
   public static void main(String[] args) {
     SpringApplication.run(BackendApplication.class, args);
-
-    logger.debug("Debug Log!");
-    logger.info("Info Log!");
-    logger.warn("Warning Log!");
-    logger.error("Error Log!");
-    logger.fatal("Fatal Log!");
 
   }
 }
