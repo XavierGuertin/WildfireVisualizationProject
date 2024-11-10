25af4e142316903ad8410768a57ce484a989e5ea
justncheng <chengjustin2002@gmail.com>
2024-11-10 10:54:05 -0500

    optimize imports

--- apps/backend/src/main/java/com/example/backend/BackendApplication.java
+++ apps/backend/src/main/java/com/example/backend/BackendApplication.java
@@ -3,10 +3,8 @@ package com.example.backend;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.Bean;
 
 /*
  * Various types of logs:
