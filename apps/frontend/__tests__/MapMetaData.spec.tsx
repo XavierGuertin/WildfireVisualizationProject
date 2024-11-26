5dc812bfc9345c816da81dce4bc285b0a6e15f8e
Kadestery <k_keati@live.concordia.ca>
2024-11-26 00:44:49 -0500

    Modified tests according to changes
    Removed loading after clicking dataset and added onLoadDataset function to mapmetadata test

--- apps/frontend/__tests__/MapMetaData.spec.tsx
+++ apps/frontend/__tests__/MapMetaData.spec.tsx
@@ -20,37 +20,51 @@ describe(MapMetaData, () => {
 
     it("meta data displays city prop", () => {
 
-        const {getByTestId} = render(<MapMetaData city='Montreal'/>);
+        const {getByTestId} = render(<MapMetaData city='Montreal' onLoadDataset={function (): void {
+            throw new Error('Function not implemented.');
+        } }/>);
         const displayedCity = getByTestId("city-div").textContent;
         expect(displayedCity).toEqual("Montreal")
     })
     it("meta data displays dataset name prop", () => {
-        const {getByTestId} = render(<MapMetaData name='Dataset1'/>)
+        const {getByTestId} = render(<MapMetaData name='Dataset1' onLoadDataset={function (): void {
+            throw new Error('Function not implemented.');
+        } }/>)
         const displayedName = getByTestId("dataset-name").textContent;
         expect(displayedName).toEqual("Dataset1")
     })
     it("meta data displays description prop", () => {
-        const {getByTestId} = render(<MapMetaData description='Desc'/>)
+        const {getByTestId} = render(<MapMetaData description='Desc' onLoadDataset={function (): void {
+            throw new Error('Function not implemented.');
+        } }/>)
         const displayedDesc = getByTestId("dataset-description").textContent;
         expect(displayedDesc).toEqual("Desc")
     })
     it("meta data displays format prop", () => {
-        const {getByTestId} = render(<MapMetaData format='CSV'/>)
+        const {getByTestId} = render(<MapMetaData format='CSV' onLoadDataset={function (): void {
+            throw new Error('Function not implemented.');
+        } }/>)
         const displayedFormat = getByTestId("dataset-format").textContent;
         expect(displayedFormat).toEqual("CSV")
     })
     it("meta data displays processes prop", () => {
-        const {getByTestId} = render(<MapMetaData processes='Process'/>)
+        const {getByTestId} = render(<MapMetaData processes='Process' onLoadDataset={function (): void {
+            throw new Error('Function not implemented.');
+        } }/>)
         const displayedProcesses = getByTestId("dataset-processes").textContent;
         expect(displayedProcesses).toEqual("Process")
     })
     it("meta data displays datasetSource prop", () => {
-        const {getByTestId} = render(<MapMetaData datasetSource='Source'/>)
+        const {getByTestId} = render(<MapMetaData datasetSource='Source' onLoadDataset={function (): void {
+            throw new Error('Function not implemented.');
+        } }/>)
         const displayedSource = getByTestId("dataset-datasource").textContent;
         expect(displayedSource).toEqual("Source")
     })
     it("all data displays in the metadata box", () => {
-        const {getByTestId} = render(<MapMetaData city='Montreal' name='Dataset1' description='Desc' format='CSV' processes='Process' datasetSource='Source'/>)
+        const {getByTestId} = render(<MapMetaData city='Montreal' name='Dataset1' description='Desc' format='CSV' processes='Process' datasetSource='Source' onLoadDataset={function (): void {
+            throw new Error('Function not implemented.');
+        } }/>)
 
         const displayedCity = getByTestId("city-div").textContent;
         expect(displayedCity).toEqual("Montreal")
@@ -73,7 +87,9 @@ describe(MapMetaData, () => {
     it("metadata box collapses when isCollapsed = false", () => {
         jest.spyOn(React, 'useState').mockImplementation(() => [true, jest.fn()])
 
-        const {getByTestId} = render(<MapMetaData />)
+        const {getByTestId} = render(<MapMetaData onLoadDataset={function (): void {
+            throw new Error('Function not implemented.');
+        } } />)
         const collapsedBox = getByTestId("collapsedMetaData");
         expect(collapsedBox).toBeInTheDocument();
     });
