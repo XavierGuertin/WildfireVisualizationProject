1ce6a21205ec14055c7288dcb216384d1a4c7670
Xavier <xavierguertin@gmail.com>
2025-02-06 20:18:48 -0500

    #166 - E2E

--- apps/frontend-e2e/src/e2e/available_datasets.cy.ts
+++ apps/frontend-e2e/src/e2e/available_datasets.cy.ts
@@ -4,6 +4,13 @@ describe('dataset', () => {
   });
 
   it('loads', () => {
+    // Input text into the prompt
+    cy.get('#swal2-input').type(
+      'https://hirondelle.crim.ca/stac/collections',
+    );
+    // Click the save button
+    cy.get('.swal2-confirm').click();
+
     // Find and click the dataset button
     const dataset = cy
       .get('[data-testid=dataset-button-1]', { timeout: 5000 })
