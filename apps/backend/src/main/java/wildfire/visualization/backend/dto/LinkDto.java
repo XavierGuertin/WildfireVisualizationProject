e36b705b25f18c89cf100ea9786cd27e09e3e5a5
Xavier Guertin <xavierguertin@gmail.com>
2025-02-27 09:56:02 -0500

    #196 - refactor[Feedback]: Unclear Purpose of File and Directory (#201)

---
+++ apps/backend/src/main/java/wildfire/visualization/backend/dto/LinkDto.java
@@ -0,0 +1,17 @@
+package wildfire.visualization.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinkDto {
+    private String href;
+    private String rel;
+    private String type;
+    private String title;
+}
