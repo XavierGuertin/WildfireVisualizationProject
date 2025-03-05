aaa20ddd587498a2a1f464b11a4e2ad1feb4b243
Xavier Guertin <xavierguertin@gmail.com>
2025-03-05 17:26:51 -0500

    #207 - Add fetchAndSaveItems endpoint

--- apps/backend/src/main/java/wildfire/visualization/backend/controller/DataController.java
+++ apps/backend/src/main/java/wildfire/visualization/backend/controller/DataController.java
@@ -114,13 +114,13 @@ public class DataController {
     }
   }
 
-  @GetMapping("/api/get-all-items/{collectionId}")
-  public ResponseEntity<List<Map<String, Object>>> fetchItems(@PathVariable("collectionId") String collectionId) {
+  @GetMapping("/api/fetch-collections-items/{collectionId}")
+  public ResponseEntity<String> fetchItems(@PathVariable("collectionId") String collectionId) {
     logger.info("Received request to fetch items");
     try {
-      List<Map<String, Object>> items = dataService.getAllItems(collectionId);
+      dataService.fetchAndSaveItems(collectionId);
       logger.debug("Successfully fetched items");
-      return ResponseEntity.ok(items);
+      return ResponseEntity.ok("Items fetched and saved successfully");
     } catch (Exception e) {
       logger.error("Error fetching items: {}", e.getMessage(), e);
       return ResponseEntity.internalServerError().body(null);
