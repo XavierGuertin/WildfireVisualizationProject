c6c08a3f8d2664cd33c03b3a44d3b1aead01d8c4
Xavier Guertin <xavierguertin@gmail.com>
2025-03-05 17:27:48 -0500

    #207 - Improve code by using variables instead of hardcoded value

--- apps/backend/src/main/java/wildfire/visualization/backend/repository/StacRepository.java
+++ apps/backend/src/main/java/wildfire/visualization/backend/repository/StacRepository.java
@@ -17,7 +17,6 @@ public class StacRepository {
   private static final Logger logger = LoggerFactory.getLogger(StacRepository.class);
   private static final String FETCHING_ALL_COLLECTIONS = "Fetching all collections";
   private static final String FETCHING_WITH_BBOX = "Fetching collections with bbox: ";
-  private static final String NO_BBOX = " (no bbox filter applied)";
 
   @Autowired
   private JdbcTemplate jdbcTemplate;
@@ -111,9 +110,9 @@ public class StacRepository {
 
     // Log query type (bbox filtering or not)
     if (bbox == null) {
-      logger.debug("Fetching all collections" + (orderBy.isEmpty() ? "" : " sorted by " + orderBy));
+      logger.debug(FETCHING_ALL_COLLECTIONS + (orderBy.isEmpty() ? "" : " sorted by " + orderBy));
     } else {
-      logger.debug("Fetching collections with bbox: [{}]", Arrays.toString(bbox));
+      logger.debug(FETCHING_WITH_BBOX + Arrays.toString(bbox));
     }
 
     try {
