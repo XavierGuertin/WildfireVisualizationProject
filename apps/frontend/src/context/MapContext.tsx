582561518f9d0b9ed02b45742f3f50f6fe37ba08
Philip Frattolillo <46691795+PFratt@users.noreply.github.com>
2025-03-07 22:13:22 -0500

    #212 - Added useMemo to the MapContext

--- apps/frontend/src/context/MapContext.tsx
+++ apps/frontend/src/context/MapContext.tsx
@@ -39,11 +39,11 @@ export const MapProvider: React.FC<PropsWithChildren> = ({children}) => {
         }
     };
  
-    return useMemo(() =>(
+    return useMemo(() => (
         <MapLayerContext.Provider value = {{ layer, setLayer, mapRef, resetView, speed, setSpeed, dataItems, setDataItems, isOnline, setIsOnline }}>
             {children}
         </MapLayerContext.Provider>
-    ),[layer, setLayer, mapRef, resetView, speed, setSpeed, dataItems, setDataItems, isOnline, setIsOnline ])
+    ),[layer, mapRef, speed, dataItems, isOnline])
 }
 
 export const useMapLayerContext = () => {
