e93aebcd57074ab409cbe340195563fcf715553c
LiamDaigle <liam.daigle@gmail.com>
2025-03-19 19:16:00 -0400

    #285 - test(e2e): Fixed dataset loading e2e test

--- apps/frontend-e2e/src/e2e/available_datasets.cy.ts
+++ apps/frontend-e2e/src/e2e/available_datasets.cy.ts
@@ -1,22 +1,28 @@
 describe('dataset', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000');
+    
   });
 
   it('loads', () => {
-    // // Input text into the prompt
-    // cy.get('#swal2-input').type(
-    //   'https://hirondelle.crim.ca/stac/collections',
-    // );
-    // // Click the save button
-    // cy.get('.swal2-confirm').click();
-    //
-    // // Find and click the dataset button
-    // const dataset = cy
-    //   .get('[data-testid=dataset-button-1]', { timeout: 5000 })
-    //   .first();
-    // dataset.click();
-    // cy.get('.metadata-container', { timeout: 10000 });
+
+    //Factory Reset before the test to avoid issue with popup not appearing after initial load
+    cy.get("[data-testid=reset-dropdown-button]").click()
+    cy.contains('button', "Factory Reset").click()
+    cy.contains('button', 'Yes', {timeout:1000}).click()
+    cy.wait(2000);  //Wait for the endpoint url to pop up
+
+    //Retrieve input the collections url into the input field and click on save
+    cy.get("#swal2-input").as("url_input");
+    cy.get('@url_input').clear().type(Cypress.env("collections_url"));
+    cy.contains('button', 'Save').click()
+
+    //Make sure notfication of success appears
+    cy.contains("div", "Collections fetched and saved successfully", {timeout: 7000}).should("be.visible")
+    cy.wait(2000)
+
+    //Assert that there are more than 0 datasets
+    cy.get("[data-testid=buttons-container]").find('button').should('not.be.empty')
   });
 
   it('collapses', () => {
