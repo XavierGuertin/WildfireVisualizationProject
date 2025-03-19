d3c5bc7920b62b80bdf3233119c779b38ab0db43
LiamDaigle <liam.daigle@gmail.com>
2025-03-19 19:29:17 -0400

    #285 - test(e2e): Fixed dataset collapse end to end tests

--- apps/frontend-e2e/src/e2e/available_datasets.cy.ts
+++ apps/frontend-e2e/src/e2e/available_datasets.cy.ts
@@ -5,7 +5,6 @@ describe('dataset', () => {
   });
 
   it('loads', () => {
-
     //Factory Reset before the test to avoid issue with popup not appearing after initial load
     cy.get("[data-testid=reset-dropdown-button]").click()
     cy.contains('button', "Factory Reset").click()
@@ -26,18 +25,27 @@ describe('dataset', () => {
   });
 
   it('collapses', () => {
-    // // Click on button to collapse available dataset
-    // cy.get('.collapse-button').click({ force: true });
-    //
-    // // Get Collapsed Container
-    // cy.get('.datasets-container', { timeout: 5000 })
-    //   .should('have.class', 'datasets-container')
-    //   .should('have.class', 'collapsed');
-    //
-    // // Click on the button to expand available dataset
-    // cy.get('.collapse-button')
-    //   .should('have.class', 'collapse-button')
-    //   .should('have.class', 'collapsed')
-    //   .click();
+    //Factory Reset before the test to avoid issue with popup not appearing after initial load
+    cy.get("[data-testid=reset-dropdown-button]").click()
+    cy.contains('button', "Factory Reset").click()
+    cy.contains('button', 'Yes', {timeout:1000}).click()
+    cy.wait(2000);  //Wait for the endpoint url to pop up
+
+    //Retrieve input the collections url into the input field and click on save
+    cy.get("#swal2-input").as("url_input");
+    cy.get('@url_input').clear().type(Cypress.env("collections_url"));
+    cy.contains('button', 'Save').click()
+
+    //Make sure notfication of success appears
+    cy.contains("div", "Collections fetched and saved successfully", {timeout: 7000}).should("be.visible")
+    cy.wait(2000)
+
+    //Click on the collapse button and assert that it collapses
+    cy.get('[data-testid=collapse-button]').click()
+    cy.get(".datasets-container").should('have.class', 'collapsed')
+
+    //Click on the collapse button and assert that it uncollapses
+    cy.get('[data-testid=collapse-button]').click()
+    cy.get(".datasets-container").should('not.have.class', 'collapsed')
   });
 });
