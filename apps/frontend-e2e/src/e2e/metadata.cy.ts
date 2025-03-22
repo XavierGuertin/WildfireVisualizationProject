a7f52691d26e8b19de278ff8c1a0d6ca3bd9ae70
LiamDaigle <liam.daigle@gmail.com>
2025-03-22 15:10:15 -0400

    #285 - test(e2e): Fixed metadata loading test

--- apps/frontend-e2e/src/e2e/metadata.cy.ts
+++ apps/frontend-e2e/src/e2e/metadata.cy.ts
@@ -2,15 +2,32 @@ describe('metadata', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000');
 
-    // const dataset = cy
-    //   .get('[data-testid=dataset-button-1]', { timeout: 5000 })
-    //   .first();
-    // dataset.click();
-    // cy.get('.metadata-container', { timeout: 10000 });
+    //Factory Reset before the test to avoid issue with popup not appearing after initial load
+    cy.get("[data-testid=reset-dropdown-button]").click()
+    cy.contains('button', "Factory Reset").click()
+    cy.contains('button', 'Yes', {timeout:1000}).click()
+    cy.wait(2000);  //Wait for the endpoint url to pop up
+
+    //Retrieve input the collections url into the input field and click on save
+    cy.get("#swal2-input").as("url_input");
+    cy.get('@url_input').clear().type(Cypress.env("collections_url"));
+    cy.contains('button', 'Save').click()
+
+    //Make sure notfication of success appears
+    cy.contains("div", "Collections fetched and saved successfully", {timeout: 7000}).should("be.visible")
+    cy.wait(2000)
   });
   it('loads', () => {
-    // cy.get('.metadata-container').should('have.length', 1);
-    // cy.get('.metadata-container').first().should('be.visible');
+
+    //Select a dataset
+    cy.get('.dataset-button').first().click()
+
+    //Check to make sure all metadata exists
+    cy.get('[data-testid=dataset-description]').invoke('text').should('not.be.empty');
+    cy.get('[data-testid=dataset-format]').invoke('text').should('not.be.empty');
+    cy.get('[data-testid=dataset-processes]').invoke('text').should('not.be.empty');
+    cy.get('[data-testid=dataset-datasource]').invoke('text').should('not.be.empty');
+
   });
   it('collapses', () => {
     // cy.get('.metadata-container').first().should('be.visible');
