14bf6c02f866fb0c5e994837d7b5754d69ba3670
Fetyali7 <fetyali7@gmail.com>
2025-03-25 00:56:12 -0400

    #291 - Fixed MapMetaData tests

--- apps/frontend/__tests__/MapMetaData.spec.tsx
+++ apps/frontend/__tests__/MapMetaData.spec.tsx
@@ -228,7 +228,8 @@ describe('MapMetaData', () => {
     });
 
     expect(toast.error).toHaveBeenCalledWith(
-      expect.stringContaining('Error loading dataset:'),
+      expect.stringContaining('error_loading_dataset:'),
+      expect.objectContaining({ toastId: 'loading-dataset-error' })
     );
   });
 
