1048d4e291b9831156ac3e84718adfc23eb2351c
LiamDaigle <liam.daigle@gmail.com>
2025-03-26 13:19:36 -0400

    #285 - fix(e2e): Added longer timeouts so pipeline can retrieve the required element

--- apps/frontend-e2e/src/e2e/metadata.cy.ts
+++ apps/frontend-e2e/src/e2e/metadata.cy.ts
@@ -5,7 +5,7 @@ describe('metadata', () => {
     //Factory Reset before the test to avoid issue with popup not appearing after initial load
     cy.get("[data-testid=reset-dropdown-button]").click()
     cy.contains('button', "Factory Reset").click({force: true})
-    cy.contains('button', 'Yes', {timeout:1000}).click()
+    cy.contains('button', 'Yes', {timeout:3000}).click()
     cy.wait(2000);  //Wait for the endpoint url to pop up
 
     //Retrieve input the collections url into the input field and click on save
