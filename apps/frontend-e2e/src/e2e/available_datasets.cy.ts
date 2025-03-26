e318a85e7d1942619a821af083cfd2f9725f18bc
LiamDaigle <liam.daigle@gmail.com>
2025-03-26 13:41:53 -0400

    #285 - fix(e2e): Added longer timeout and extra check to fix error with button disappearing before being able to click

--- apps/frontend-e2e/src/e2e/available_datasets.cy.ts
+++ apps/frontend-e2e/src/e2e/available_datasets.cy.ts
@@ -8,7 +8,7 @@ describe('dataset', () => {
     //Factory Reset before the test to avoid issue with popup not appearing after initial load
     cy.get("[data-testid=reset-dropdown-button]").click()
     cy.contains('button', "Factory Reset").click({force: true})
-    cy.contains('button', 'Yes', {timeout:1000}).click()
+    cy.contains('button', 'Yes', {timeout: 10000}).should('be.visible').click()
     cy.wait(2000);  //Wait for the endpoint url to pop up
 
     //Retrieve input the collections url into the input field and click on save
@@ -28,7 +28,7 @@ describe('dataset', () => {
     //Factory Reset before the test to avoid issue with popup not appearing after initial load
     cy.get("[data-testid=reset-dropdown-button]").click()
     cy.contains('button', "Factory Reset").click({force: true})
-    cy.contains('button', 'Yes', {timeout:3000}).click()
+    cy.contains('button', 'Yes', {timeout: 10000}).should('be.visible').click()
     cy.wait(2000);  //Wait for the endpoint url to pop up
 
     //Retrieve input the collections url into the input field and click on save
