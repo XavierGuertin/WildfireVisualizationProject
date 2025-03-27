3c506ba7cef0dc8d58df85bcafae7a6597f0ef22
LiamDaigle <liam.daigle@gmail.com>
2025-03-27 12:00:27 -0400

    #285 - fix(e2e): Added screenshot functionality on fail

--- apps/frontend-e2e/cypress.config.ts
+++ apps/frontend-e2e/cypress.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
     ...nxE2EPreset(__filename, {
       cypressDir: 'apps/frontend-e2e/src',
     }),
+    screenshotOnRunFailure: true,
+    screenshotsFolder: 'cypress/screenshots',
     baseUrl: 'http://127.0.0.1:3000', // Set only if NX doesn’t set it automatically
     setupNodeEvents(on, config) {
       coverageTask(on, config); // Enable code coverage tasks
