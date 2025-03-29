cc2cbd663d92cdc42c8f803b083ac689c7fc6973
LiamDaigle <liam.daigle@gmail.com>
2025-03-29 13:12:08 -0400

    #285 - test(e2e): Created tests for the simulation

--- apps/frontend-e2e/src/e2e/simulation.cy.ts
+++ apps/frontend-e2e/src/e2e/simulation.cy.ts
@@ -26,25 +26,78 @@ describe('simulation', () => {
       cy.wait(2000)
 
       //Load montreal_2023 dataset
-      cy.get('[data-testid=dataset-button-montreal_2023]').should('exist').click({force: true})
+      cy.get('[data-testid=dataset-button-newyork_2024]').should('exist').click({force: true})
       cy.get('[data-testid=load-dataset-button]').should('exist').click({force: true})
       cy.get('button').contains('Yes').should('exist').click({force: true})
+      
+      cy.contains('div', 'Collection\'s items have been fetched successfully', {timeout:20000}).should('be.visible')
     });
 
     it('runs properly', () => {
+      cy.get('div.timeMarkerThumb').then(($element) => {
+        const initialPosition = $element[0].getBoundingClientRect();
 
+        cy.get('[data-testid="play-pause-button"]').click()
+
+        cy.wait(8000)
+        
+        cy.get('[data-testid="play-pause-button"]').click()
+
+        cy.get('div.timeMarkerThumb').then(($currentElement) => {
+
+          const currentPosition = $currentElement[0].getBoundingClientRect()
+
+          expect(currentPosition.left).to.be.greaterThan(initialPosition.left)
+        })
+      })
     })
 
     it('runs properly when sped up', () => {
+      cy.get('div.timeMarkerThumb').then(($element) => {
+        const initialPosition = $element[0].getBoundingClientRect();
 
+        cy.get('[data-testid="speed-point-1.5"]').click()
+
+        cy.get('[data-testid="play-pause-button"]').click()
+
+        cy.wait(8000)
+        
+        cy.get('[data-testid="play-pause-button"]').click()
+
+        cy.get('div.timeMarkerThumb').then(($currentElement) => {
+
+          const currentPosition = $currentElement[0].getBoundingClientRect()
+
+          expect(currentPosition.left).to.be.greaterThan(initialPosition.left)
+        })
+      })
     })
 
     it('runs properly when slowed down', () => {
+      cy.get('div.timeMarkerThumb').then(($element) => {
+        const initialPosition = $element[0].getBoundingClientRect();
 
+        cy.get('[data-testid="speed-point-0.5"]').click()
+
+        cy.get('[data-testid="play-pause-button"]').click()
+
+        cy.wait(16000)
+        
+        cy.get('[data-testid="play-pause-button"]').click()
+
+        cy.get('div.timeMarkerThumb').then(($currentElement) => {
+
+          const currentPosition = $currentElement[0].getBoundingClientRect()
+
+          expect(currentPosition.left).to.be.greaterThan(initialPosition.left)
+        })
+      })
     })
 
-    it('displays weather assets properly', () => {
-      
+    it('weather assets load properly', () => {
+      cy.contains('div', 'Collection\'s assets have been fetched successfully', {timeout:60000})
+
+      cy.get('div.weather-dropdown').should('exist')
     })
 
   });
